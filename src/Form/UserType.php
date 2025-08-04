3cb7cf010af0f0508b483f71095d20fad26df354
Elodie Mazué <elodie.mazue2025@campus-eni.fr>
2025-08-04 17:47:28 +0200

    Affichage formulaire en cours

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,25 +2,61 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('campus')
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('pseudo')
-            ->add('telephone')
-            ->add('actif')
+            ->add('campus', EntityType::class, [
+                'label' => 'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+                'expanded' => false,
+                'multiple' => false,
+                'disabled'=> !$options['isAdmin'],
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Telephone',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ] )
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez choisir un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ]
+            ])
         ;
     }
 
