bc4d4963e52dea0cddcadda8edcb65d281a299c9
Elodie Mazué <elodie.mazue2025@campus-eni.fr>
2025-08-04 09:26:46 +0200

    Installation de Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,9 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap'; // Déclaration du JS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'; // Déclaration du CSS de Bootstrap
+import './styles/app.css'; // Fichier CSS personnalisé
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
