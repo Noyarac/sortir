037c3ab8cd54584936cd8f0f66c8b502769c9099
Elodie Mazué <elodie.mazue2025@campus-eni.fr>
2025-08-04 15:31:16 +0200

    Personnalisation du formulaire de connexion et ajout de la fonction Se souvenir de moi

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ final class MainController extends AbstractController
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
-        return $this->render('main/home.html.twig',
+        return $this->render('main/home.html.twig'
         );
     }
 }
