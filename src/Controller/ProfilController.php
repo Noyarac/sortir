e0717511a6034bab9e61994670627af73477462c
Elodie Mazué <elodie.mazue2025@campus-eni.fr>
2025-08-05 15:35:26 +0200

    Fonctionnalité : afficher profil utilisateur ok

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,32 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+#[Route('/profil')]
 final class ProfilController extends AbstractController
 {
-    #[Route('/profil/modifier', name: 'profil_modifier', methods: ['GET','POST'])]
+    #[Route('/modifier', name: 'profil_modifier', methods: ['GET','POST'])]
     public function modifierProfil(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
+        //Inutile de vérifier que $user existe car application entièrement protégée et seulement accessible à ROLE_USER
         $userForm = $this->createForm(UserType::class, $user, [
-            'campusModifiable' => false,
+            'campusModifiable' => false, //un utilisateur ne peut pas modifier son campus de référence
         ]);
 
         $userForm->handleRequest($request);
 
         if ($userForm->isSubmitted() && $userForm->isValid()) {
-            $entityManager->persist($user);
             $entityManager->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès!');
             return $this->redirectToRoute('main_home');
         }
 
         return $this->render('profil/modifierProfil.html.twig', [
             'userForm' => $userForm,
+        ]);
+    }
+    #[Route('/{id}', name: 'profil_afficher', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    public function afficherProfil(User $user): Response
+    {
+        return $this->render('profil/afficherProfil.html.twig', [
+            'user'=>$user,
         ]);
     }
 }
