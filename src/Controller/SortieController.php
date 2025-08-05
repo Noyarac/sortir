826a702ab13b762632a3e59127d9e70f80f9236f
Benjamin <benjamin@carayon.eu>
2025-08-05 16:45:13 +0200

    affichage détails sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -13,10 +13,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route("/sortie")]
 final class SortieController extends AbstractController
 {
-    #[Route('/{id}', name: 'sortie_details')]
+    #[Route('/{id}', name: 'sortie_details', requirements: ["id" => "\d+"])]
     public function details(Sortie $sortie): Response
     {
         return $this->render('sortie/details.html.twig', [
+            "sortie" => $sortie,
         ]);
     }
 
