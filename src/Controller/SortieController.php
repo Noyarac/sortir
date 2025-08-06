3a5822f957e3f75cf45d65b24e70472f1e2e42b7
Benjamin <benjamin@carayon.eu>
2025-08-06 11:33:27 +0200

    creation voter

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,10 +5,14 @@ namespace App\Controller;
 use App\Entity\Sortie;
 use App\Form\CampusType;
 use App\Repository\SortieRepository;
+use App\Security\Voter\SortieVoter;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route("/sortie")]
 final class SortieController extends AbstractController
@@ -19,6 +23,17 @@ final class SortieController extends AbstractController
         return $this->render('sortie/details.html.twig', [
             "sortie" => $sortie,
         ]);
+    }
+
+    #[Route('/{id}/inscription', name: 'sortie_inscription', requirements: ["id" => "\d+"])]
+    #[IsGranted(SortieVoter::INSCRIPTION, 'sortie')]
+    public function inscription(Sortie $sortie, UserInterface $user, EntityManagerInterface $em): Response
+    {
+        $sortie->addParticipant($user);
+        $em->persist($sortie);
+        $em->flush();
+        dd($sortie);
+        return $this->redirectToRoute("main_home");
     }
 
 }
---
+++ src/Security/Voter/SortieVoter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Etat;
+use App\Entity\Sortie;
+use App\Entity\User;
+use DateTimeImmutable;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class SortieVoter extends Voter
+{
+    public const INSCRIPTION = 'sortie_inscription';
+    public const DESISTEMENT = 'sortie_desistement';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return in_array($attribute, [self::INSCRIPTION, self::DESISTEMENT])
+            && $subject instanceof \App\Entity\Sortie;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $sortie, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::INSCRIPTION:
+                return $this->inscription($sortie, $user);
+                break;
+
+            case self::DESISTEMENT:
+                return $this->desistement($sortie, $user);
+                break;
+        }
+        return false;
+    }
+
+    private function inscription(Sortie $sortie, User $user) : bool {
+        return
+            !in_array($user, $sortie->getParticipants()->getValues())
+            && $sortie->getDateLimiteInscription() <= new DateTimeImmutable()
+            && $sortie->getEtat() == Etat::OUVERTE
+            && sizeof($sortie->getParticipants()) < $sortie->getNbInscriptionMax();
+    }
+
+    private function desistement(Sortie $sortie, User $user) : bool {
+        return
+            in_array($user, $sortie->getParticipants()->getValues())
+            && in_array($sortie->getEtat(), [Etat::OUVERTE, Etat::CLOTUREE])
+            && $sortie->getDateHeureDebut() < new DateTimeImmutable();
+    }
+}
