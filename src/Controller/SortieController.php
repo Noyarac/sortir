ed0e27ff76b4ecc9b83a08a27384bdff28957d35
Benjamin <benjamin@carayon.eu>
2025-08-06 15:12:57 +0200

    nettoyage de use

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,16 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Sortie;
-use App\Form\CampusType;
-use App\Repository\SortieRepository;
 use App\Security\Voter\SortieVoter;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route("/sortie")]
 final class SortieController extends AbstractController
