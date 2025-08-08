44bb49934a43d0d873487b3dfe1e93fe70921ab3
Benjamin <benjamin@carayon.eu>
2025-08-08 10:55:07 +0200

    UT Sortie

--- tests/Unit/Entity/SortieTest.php
+++ tests/Unit/Entity/SortieTest.php
@@ -2,12 +2,19 @@
 
 namespace App\Tests\Unit\Entity;
 
+use App\Entity\Etat;
+use App\Entity\Sortie;
+use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
 class SortieTest extends TestCase
 {
-    public function testSomething(): void
+    public function testCorrectionDeLEtatOuverteEnClotureeSiLaDateLimiteEstDepassee(): void
     {
-        $this->assertTrue(true);
+        $sortie = new Sortie();
+        $sortie->setEtat(Etat::OUVERTE->value);
+        $sortie->setDateLimiteInscription(new DateTimeImmutable("yesterday"));
+
+        $this->assertEquals(Etat::CLOTUREE->value, $sortie->getEtat());
     }
 }
