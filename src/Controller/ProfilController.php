4845358368363c21cc2c680b85f2eb06d4acd85b
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-08-11 14:17:27 +0200

    profil picture fonctionnelle

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,16 +6,22 @@ use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 #[Route('/profil')]
 final class ProfilController extends AbstractController
 {
-    #[Route('/modifier', name: 'profil_modifier', methods: ['GET','POST'])]
-    public function modifierProfil(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    #[Route('/modifier', name: 'profil_modifier', methods: ['GET', 'POST'])]
+    public function modifierProfil(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, SluggerInterface $slugger): Response
     {
+        /** @var User $user */
         $user = $this->getUser();
         //Inutile de vérifier que $user existe car application entièrement protégée et seulement accessible à ROLE_USER
         $userForm = $this->createForm(UserType::class, $user, [
@@ -26,10 +32,37 @@ final class ProfilController extends AbstractController
 
         if ($userForm->isSubmitted() && $userForm->isValid()) {
             $plainPassword = $userForm->get('plainPassword')->getData();
-            if($plainPassword){
+            if ($plainPassword) {
                 $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
                 $user->setPassword($hashedPassword);
             }
+
+            /** @var UploadedFile $image */
+            $imageFile = $userForm->get('image')->getData();
+            if ($imageFile) {
+                try {
+                    $imageFile->move(
+                        $this->getParameter('app.backendProfilePicturesDirectory'),
+                        $slugger->slug($user->getId()));
+                } catch (FileException $e) {
+                    $this->addFlash('danger', "Un problème est survenu lors de l'enregistrement de votre image de profil.");
+                }
+            }
+
+            if ($userForm->get("deleteImage")->getViewData()) {
+                $fileSystem = new Filesystem();
+                $filePath = $this->getParameter('app.backendProfilePicturesDirectory') . '/' . $user->getId();
+                if ($fileSystem->exists($filePath)) {
+                    try {
+                        $fileSystem->remove($filePath);
+                    } catch (IOExceptionInterface $exception) {
+                        $this->addFlash("danger", "Impossible de supprimer votre image de profile.");
+                    }
+                } else {
+                    $this->addFlash("info", "Votre image de profile n'a pas été supprimée, car elle n'existe pas.");
+                }
+            }
+
             $entityManager->flush();
             $this->addFlash('success', 'Profil mis à jour avec succès!');
             return $this->redirectToRoute('main_home');
@@ -37,13 +70,14 @@ final class ProfilController extends AbstractController
 
         return $this->render('profil/modifierProfil.html.twig', [
             'userForm' => $userForm,
+            'user' => $user,
         ]);
     }
-    #[Route('/{id}', name: 'profil_afficher', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    #[Route('/{id}', name: 'profil_afficher', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function afficherProfil(User $user): Response
     {
         return $this->render('profil/afficherProfil.html.twig', [
-            'user'=>$user,
+            'user' => $user,
         ]);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,28 +6,58 @@ use App\Entity\Campus;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-       $builder
+        $builder
+            ->add(
+                'deleteImage',
+                CheckboxType::class,
+                [
+                    "label" => "Supprimer l'image de profil.",
+                    "mapped" => false,
+                    "required" => false,
+                ]
+            )
+            ->add(
+                'image',
+                FileType::class,
+                [
+                    "mapped" => false,
+                    "required" => false,
+                    "constraints" => [
+                        new Image([
+                            "maxSize" => "6000k",
+                            "mimeTypes" => [
+                                "image/jpeg",
+                                "image/png",
+                                "image/jpeg",
+                            ],
+                            "mimeTypesMessage" => "Veuillez utiliser un JPEG ou un PNG.",
+                        ])
+                    ]
+                ]
+            )
             ->add('campus', EntityType::class, [
                 'label' => 'Campus',
                 'class' => Campus::class,
                 'choice_label' => 'nom',
                 'expanded' => false,
                 'multiple' => false,
-                'disabled'=> !$options['campusModifiable'],
+                'disabled' => !$options['campusModifiable'],
             ])
             ->add('pseudo', TextType::class, [
                 'label' => 'Pseudo',
@@ -56,7 +86,7 @@ class UserType extends AbstractType
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Email',
-            ] )
+            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les mots de passe doivent correspondre.',
@@ -82,11 +112,11 @@ class UserType extends AbstractType
         ;
     }
 
-   public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
-            'campusModifiable' => false,//valeur par défaut false : le campus n'est pas modifiable par un utilisateur
+            'campusModifiable' => false, //valeur par défaut false : le campus n'est pas modifiable par un utilisateur
         ]);
     }
 }
