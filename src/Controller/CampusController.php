2da07579cf18ae4566cbe87d75a4b9bacde9e0cc
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-08-12 11:36:34 +0200

    suppression campus

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -7,6 +7,7 @@ use App\Form\CampusType;
 use App\Form\DTO\FiltreCampus;
 use App\Form\FiltreCampusType;
 use App\Repository\CampusRepository;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,9 +15,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_ADMIN")]
 final class CampusController extends AbstractController
 {
-    #[IsGranted("ROLE_ADMIN")]
     #[Route('/admin/campus', name: 'admin_campus_list')]
     public function adminCampusList(Request $request, CampusRepository $campusRepository, EntityManagerInterface $em): Response
     {
@@ -49,4 +50,28 @@ final class CampusController extends AbstractController
             compact("allCampus", "filtreForm", "newCampusForm")
         );
     }
+
+    #[Route('/admin/campus/{id}/supprimer', name: 'admin_campus_supprimer', requirements: ['id'=>'\d+'], methods: ['POST'])]
+    public function adminCampusSupprimer(Campus $campus, Request $request, SortieRepository $sortieRepository, EntityManagerInterface $em): Response
+    {
+        // Vérification token CSRF
+        $submittedToken = $request->getPayload()->get('_token');
+        if (!$this->isCsrfTokenValid('suppression_campus_' . $campus->getId(), $submittedToken)) {
+            $this->addFlash("danger", "Le token CSRF n'est pas valide.");
+            return $this->redirectToRoute("admin_campus_list");
+        }
+
+        // Vérification de l'existence de sorties reliées au campus
+        if (sizeof($sortieRepository->findBy(["campus" => $campus]))) {
+            $this->addFlash("info", "Vous ne pouvez pas supprimer ce campus ; des sorties en dépendent.");
+            return $this->redirectToRoute("admin_campus_list");
+        }
+
+        $em->remove($campus);
+        $em->flush();
+        $this->addFlash('success', $campus->getNom(). ' a bien été supprimé');
+
+        return $this->redirectToRoute("admin_campus_list");
+    }
+
 }
