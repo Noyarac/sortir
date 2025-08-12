d7f061c3c5a4ee58f92ce4038424f299d1581100
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-08-12 14:06:25 +0200

    homogeneisation du style admin + ajout light/dark themes

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/campus')]
 final class CampusController extends AbstractController
