c824c25c5e97ab1a3b8779cd4cfa19b4f3157b2b
Elodie Mazué <elodie.mazue2025@campus-eni.fr>
2025-08-14 10:00:26 +0200

    Commit avant mise à jour branche principale

---
+++ src/Form/LieuType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use App\Repository\VilleRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'minlength' => 3,
+                    'maxlength' => 50,
+                ]
+            ] )
+            ->add('rue', TextType::class, [
+                'label' => 'Rue',
+                'attr' => [
+                    'minlength' => 3,
+                    'maxlength' => 255,
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+                'label' => 'Latitude',
+                'attr' => [
+                    'min'=> -90,
+                    'max'=> 90,
+                    'step'=> '0.000001'
+                ]
+            ])
+            ->add('longitude', NumberType::class, [
+                'label' => 'Longitude',
+                'attr' => [
+                    'min'=> -180,
+                    'max'=> 180,
+                    'step'=> '0.000001'
+                ]
+            ])
+            ->add('ville', EntityType::class, [
+                'label' => 'Ville',
+                'class' => Ville::class,
+                'choice_label' => 'nom',
+                'placeholder' => 'Choisissez une ville',
+                'query_builder' => function (VilleRepository $villeRepository) {
+                    return $villeRepository->createQueryBuilder('v')
+                        ->orderBy('v.nom', 'ASC');
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
